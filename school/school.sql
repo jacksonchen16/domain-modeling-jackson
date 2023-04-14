ebfbceb73f55c5835d72081c42ef717b924cf9cf
Jackson Chen <jackson.chen@kellogg.northwestern.edu>
2023-04-14 18:55:23 +0000

    DOMAIN Modeling building

--- school/school.sql
+++ school/school.sql
@@ -20,18 +20,18 @@ CREATE TABLE teachers (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT,
   last_name TEXT,
-  bio TEXT
+  bio TEXT,
 )
 
-CREATE TABLE courses (
+CREATE TABLE teachers (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
-  description TEXT
+  description TEXT,
 )
 
 CREATE TABLE sections (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   time TEXT,
   course_id INTEGER,
-  teachER_id INTEGER,
-); 
+  teacher_id INTEGER,
+);
